d0d4e2430ab571816010f48d12f987a6059fca68
Yarolsav <yaspichihin@gmail.com>
2024-06-15 14:53:42 +0300

    add persistor

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import { App } from "./App";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store.js
+++ src/store.js
@@ -1,15 +1,44 @@
 import logger from "redux-logger";
 import { configureStore, nanoid } from "@reduxjs/toolkit";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+
 import { tasksReducer } from "./features/Tasks/tasks-slice";
 import { filterReducer } from "./features/Filter/filter-slice";
 
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filter: filterReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
   devTools: true,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
   preloadedState: { tasks: [{ id: nanoid(), title: "PreloadedTask", completed: false }] },
 });
+
+export const persistor = persistStore(store);
